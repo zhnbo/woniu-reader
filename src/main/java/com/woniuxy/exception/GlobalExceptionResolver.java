ea51f1f5f22f92fd9f28b81312e3bd6ab7dbd08f
Zhnoo <private_zho@foxmail.com>
2020-10-12 14:54:24 +0800

    修复BUG

--- src/main/java/com/woniuxy/exception/GlobalExceptionResolver.java
+++ src/main/java/com/woniuxy/exception/GlobalExceptionResolver.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.bind.ValidationException;
+import javax.validation.ValidationException;
 
 
 /**
